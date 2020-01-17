9a7d38defc15678f4c2c39e43d3571c6206a7565
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2020-01-17 07:16:40 +0300

    adding delete functionality and single page for the post

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import MenuBar from "./components/Menubar";
+import SinglePost from './components/pages/SinglePost';
 
 import { Container } from 'semantic-ui-react'
 import { AuthProvider } from "./context/auth";
@@ -21,9 +22,11 @@ function App() {
     <Router>
       <Container>
       <MenuBar />
+      >
       <Route exact path="/" component={Home} />
       <AuthRoute exact path='/login' component={Login} />
       <AuthRoute exact path='/register' component={Register} />
+      <Route exact path='/posts/:postId' component={SinglePost} />
       </Container>
     </Router>
     </AuthProvider>
@@ -31,3 +34,4 @@ function App() {
 }
 
 export default App;
+ 
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 const PostCard = ( {post: {body, createdAt, id, username, likeCount, commentCount, likes}}) => {
     
@@ -34,15 +35,11 @@ const PostCard = ( {post: {body, createdAt, id, username, likeCount, commentCoun
       <Button color='blue' basic>
         <Icon name='comments' />
       </Button>
-      <Label as='a' basic color='blue' pointing='left'>
+      <Label basic color='blue' pointing='left'>
         {commentCount}
       </Label>
     </Button>
-    {user && user.username === username && (
-      <Button floated="right" as='div' color='red' onClick={() => console.log('Delete Post')}>
-        <Icon name='trash' style={{margin: 0}}></Icon>
-      </Button>
-    )}
+    {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
     )
